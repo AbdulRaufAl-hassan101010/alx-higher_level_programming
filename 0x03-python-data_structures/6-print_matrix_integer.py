7f90f15ac26b7f0e2d32c1ef6f0e21e604e9f27b
AbdulRaufAl-hassan101010 <abdulraufalhassan101010@gmail.com>
2022-12-05 20:40:52 +0000

    6-print_matrix_integer.py

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    if matrix:
+        for row in matrix:
+            column_length = len(row)
+            for i in range(column_length):
+                if column_length - 1 == i:
+                    print(row[i], end="")
+                else:
+                    print(row[i], end=" ")
+            print()
+
+
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+
+print_matrix_integer(matrix)
+print("--")
+print_matrix_integer()
