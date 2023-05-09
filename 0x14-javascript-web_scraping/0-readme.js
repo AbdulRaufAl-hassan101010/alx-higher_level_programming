32cd63ff12c7a6337c465f2e64eb140f69ee0671
Abdul-Rauf Alhassan <94212640+AbdulRaufAl-hassan101010@users.noreply.github.com>
2023-05-09 20:58:45 +0000

    Update 0-readme.js

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,13 +1,5 @@
 #!/usr/bin/node
-
 const fs = require('fs');
-
-const filePath = process.argv[2];
-
-fs.readFile(filePath, 'utf-8', (err, data) => {
-  if (err) {
-    console.log(err);
-    return;
-  }
-  console.log(data);
-})
+fs.readFile(process.argv[2], 'utf-8', (error, data) => {
+  console.log(error || data);
+});
